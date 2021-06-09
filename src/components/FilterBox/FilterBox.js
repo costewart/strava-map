381a2adc0004e1402ebd6bade90db7a0d0670de9
charlottestewart <costewart4@gmail.com>
2021-06-08 19:56:21 -0700

    styled the colors a bit, made default value correct

--- src/components/FilterBox/FilterBox.js
+++ src/components/FilterBox/FilterBox.js
@@ -3,6 +3,20 @@ import SportCheckbox from "../SportCheckbox";
 import "./FilterBox.css";
 
 const FilterBox = ({ onSubmit, sports }) => {
+  let colors = {
+    Ride: "#6495ED",
+    Run: "#E9967A",
+    Swim: "#1E90FF",
+    Walk: "#DAA520",
+    Hike: "#228B22",
+    AlpineSki: "#DCDCDC",
+    BackcountrySki: "#DCDCDC",
+    Canoe: "#B22222",
+    Crossfit: "#2F4F4F",
+    Kitesurf: "#8FBC8F",
+    Surfing: "#556B2F",
+  };
+
   return (
     <div className="filter_box">
       <h1>Edit Your Map</h1>
@@ -10,7 +24,12 @@ const FilterBox = ({ onSubmit, sports }) => {
         <div className="checkboxes">
           <p>Type of Sport</p>
           {Object.keys(sports).map((sport) => (
-            <SportCheckbox sport={sport} onSubmit={onSubmit} sports={sports} />
+            <SportCheckbox
+              sport={sport}
+              sportColor={colors[sport]}
+              onSubmit={onSubmit}
+              sports={sports}
+            />
           ))}
         </div>
         <p>Time Period </p>
--- src/components/SportCheckbox/SportCheckbox.js
+++ src/components/SportCheckbox/SportCheckbox.js
@@ -1,8 +1,9 @@
 import React, { useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectors, actions } from "../../redux/activities";
+import "./SportCheckbox.css";
 
-const SportCheckbox = ({ sport, onSubmit, sports }) => {
+const SportCheckbox = ({ sport, sportColor, onSubmit, sports }) => {
   const dispatch = useDispatch();
   const color = useRef(null);
 
@@ -23,23 +24,24 @@ const SportCheckbox = ({ sport, onSubmit, sports }) => {
   };
 
   return (
-    <div>
-      <label>
-        <input
-          type="checkbox"
-          onChange={handleToggle}
-          key={sport}
-          name={sport}
-          checked={sports[sport]}
-        />
-        {sport}
-      </label>
+    <div className="sport">
       <input
         type="color"
         id="color-picker"
         ref={color}
+        defaultValue={sportColor}
         onChange={() => changeColor(color.current.value)}
       />
+
+      <div className="sport-title">{sport}</div>
+      <input
+        type="checkbox"
+        onChange={handleToggle}
+        key={sport}
+        name={sport}
+        checked={sports[sport]}
+      />
+
       <span class="select"></span>
     </div>
   );
